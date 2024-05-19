0126918914910806daac236417afa0ac5f6dcc90
frameton <frameton@student.42.fr>
2024-05-20 01:48:31 +0200

    fix

--- expert-system.py
+++ expert-system.py
@@ -3,6 +3,8 @@ from home import *
 import argparse
 from get_gen_params import *
 from save_params import *
+import sys
+
 
 
 if __name__ == "__main__":
@@ -21,5 +23,9 @@ if __name__ == "__main__":
     params["path"] = args.p
     params = expert_system_program(params)
     save_params(params)
+
+  elif len(sys.argv) == 1:
+    print("usage: expert-system.py [-h] [-a] [-p P]")
+    print("       -h for more informations.")
 
   exit()
